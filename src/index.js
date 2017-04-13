cfea65107e579911dd1734dff613add0d538b5c0
MartinCrane <martin.crane@gmail.com>
2017-04-13 14:15:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import manageRestaurants from './reducers/manageRestaurants';
 
+import { comineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  books: booksReducer,
+  authors: authorsReducer
+});
+
+export const store = createStore(rootReducer);
+
+function booksReducer(state = [], action) {
+  switch (action.type) {
+
+     case "ADD_BOOK":
+      return state.concat(action.book);
+
+    case "REMOVE_BOOK":
+      const idx = state.indexOf(action.id);
+      return [ ...state.slice(0, idx), ...state.slice(idx + 1) ];
+
+    default:
+      return state;
+  }
+}
+
+function authorsReducer(state = [], action) {
+  switch (action.type) {
+
+    case "ADD_AUTHOR":
+      return state.concat(action.author);
+
+    case "REMOVE_AUTHOR":
+      const idx = state.indexOf(action.id);
+      return [ ...state.slice(0, idx), ...state.slice(idx + 1) ];
+
+    default:
+      return state;
+  }
+}
