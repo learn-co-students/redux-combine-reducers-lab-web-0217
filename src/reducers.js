61b188062e66979dec3ac8031d6de14810613954
Daniel <daniel.seehausen@gmail.com>
2017-04-13 11:17:21 -0400

    Done.

---
+++ src/reducers.js
@@ -0,0 +1,33 @@
+
+export function books(state=[], action) {
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [].concat(state, action.payload)
+    case "REMOVE_BOOK":
+      return [].concat(state.filter((b) => b !== action.payload))
+    default:
+      return state
+  }
+}
+
+export function recommendedBooks(state=[], action) {
+  switch (action.type) {
+    case "ADD_RECOMMENDED_BOOK":
+      return [].concat(state, action.payload)
+    case "REMOVE_RECOMMENDED_BOOK":
+      return [].concat(state.filter((b) => b !== action.payload))
+    default:
+      return state
+  }
+}
+
+export function combineReducers(reducers){
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key)=>{
+        nextState[key] = reducers[key](state[key], action);
+        return nextState
+      }, {}
+    )
+  }
+}
