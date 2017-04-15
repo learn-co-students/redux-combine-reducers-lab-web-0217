3951108ef2371a3ca8f357ed5993819d4f5fe058
nanyr <nanyramirez0@gmail.com>
2017-04-15 16:18:25 -0400

    Done.

---
+++ src/reducers.js
@@ -0,0 +1,39 @@
+
+
+export function combineReducers(reducers){
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key)=>{
+        nextState[key] = reducers[key](state[key], action);
+        return nextState
+      }, {}
+    )
+  }
+}
+
+
+export function books(state=[], action){
+  switch(action.type){
+    case 'ADD_BOOK':
+      return [].concat(state, action.payload)
+    case 'REMOVE_BOOK':
+        let i= state.indexOf(action.payload)
+        return [].concat(state.slice(0, i)), state.slice(i+1)
+    default:
+    return state
+  }
+
+}
+
+export function recommendedBooks(state=[], action){
+  switch(action.type){
+    case 'ADD_RECOMMENDED_BOOK':
+      return [].concat(state, action.payload)
+    case 'REMOVE_RECOMMENDED_BOOK':
+      let i= state.indexOf(action.payload)
+        return [].concat(state.slice(0, i)), state.slice(i+1)
+    default:
+    return state
+  }
+
+}
